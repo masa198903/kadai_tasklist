dd37ae0171bc2fb5e147229cfd28b3788937d6c7
masahiko.hirose <nocturne.9.luna@gmail.com>
2022-08-13 16:20:59 +0900

    L16課題のIndexServletを作成

---
+++ src/lesson16/L16IndexServlet.java
@@ -0,0 +1,32 @@
+package lesson16;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class L16IndexServlet
+ */
+@WebServlet(name = "index", urlPatterns = { "/index" })
+public class L16IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public L16IndexServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
