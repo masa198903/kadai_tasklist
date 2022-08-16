79b364bca40a07d333aca76628abc7a44723ad70
masahiko.hirose <nocturne.9.luna@gmail.com>
2022-08-16 11:08:17 +0900

    Index/NewServlet16の修正

--- src/lesson16/NewServlet16.java
+++ src/lesson16/NewServlet16.java
@@ -1,9 +1,8 @@
 package lesson16;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models16.Task;
-import utils16.DBUtil16;
 
 /**
  * Servlet implementation class NewServlet16
@@ -31,28 +29,36 @@ public class NewServlet16 extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil16.createEntityManager();
-        em.getTransaction().begin();
+        //CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
 
-        Task t = new Task();
+        // おまじないとしてのインスタンス生成
+        request.setAttribute("task", new Task());
 
-        String title = "taro";
-        t.setTitle(title);
-
-        String content = "hellow";
-        t.setContent(content);
-
-        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-        t.setCreated_at(currentTime);
-        t.setUpdated_at(currentTime);
-
-        // データベースに保存
-        em.persist(t);
-        em.getTransaction().commit();
-
-        // 自動採番されたIDの値を取得
-        response.getWriter().append(Integer.valueOf(t.getId()).toString());
-
-        em.close();
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views16/tasks/new.jsp");
+        rd.forward(request,response);
+//        EntityManager em = DBUtil16.createEntityManager();
+//        em.getTransaction().begin();
+//
+//        Task t = new Task();
+//
+//        String title = "taro";
+//        t.setTitle(title);
+//
+//        String content = "hellow";
+//        t.setContent(content);
+//
+//        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+//        t.setCreated_at(currentTime);
+//        t.setUpdated_at(currentTime);
+//
+//        // データベースに保存
+//        em.persist(t);
+//        em.getTransaction().commit();
+//
+//        // 自動採番されたIDの値を取得
+//        response.getWriter().append(Integer.valueOf(t.getId()).toString());
+//
+//        em.close();
     }
 }
