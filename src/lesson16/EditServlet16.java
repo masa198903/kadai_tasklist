f57907e814f61898d0e3a72fe9922df53e05dff3
masahiko.hirose <nocturne.9.luna@gmail.com>
2022-08-16 18:23:52 +0900

    EditServlet.javaを作成

---
+++ src/lesson16/EditServlet16.java
@@ -0,0 +1,50 @@
+package lesson16;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models16.Task;
+import utils16.DBUtil16;
+
+/**
+ * Servlet implementation class EditServlet16
+ */
+@WebServlet(name = "edit", urlPatterns = { "/edit" })
+public class EditServlet16 extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditServlet16() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil16.createEntityManager();
+
+        Task t = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        request.setAttribute("task", t);
+        request.setAttribute("_token", request.getSession().getId());
+
+        request.getSession().setAttribute("task_id", t.getId());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views16/tasks/edit.jsp");
+        rd.forward(request, response);
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
+
+}
